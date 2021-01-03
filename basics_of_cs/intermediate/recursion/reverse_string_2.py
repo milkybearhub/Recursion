2bb3d828be258bff543cd19da1d4180ffb845751
M.Odagawa <42145314+milkybearhub@users.noreply.github.com>
2021-01-03 10:24:11 +0900

    Reverse string (another solution)
    https://recursionist.io/dashboard/problems/submissions/37286

---
+++ basics_of_cs/intermediate/recursion/reverse_string_2.py
@@ -0,0 +1,7 @@
+def reverseString(string):
+    return reverseStringHelper(string[0], 1, string)
+
+# 文字列の切り取りをせずにインデックスをずらす
+def reverseStringHelper(reversedString, index, originalString):
+    if index >= len(originalString): return reversedString
+    return reverseStringHelper(originalString[index] + reversedString, index + 1, originalString)
