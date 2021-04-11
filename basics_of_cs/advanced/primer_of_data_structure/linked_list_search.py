5946fd06f72d2515b4d025446f935adec54a6387
M.Odagawa <mus.29sup.guit@gmail.com>
2021-04-12 00:51:21 +0900

    Linked list search

---
+++ basics_of_cs/advanced/primer_of_data_structure/linked_list_search.py
@@ -0,0 +1,14 @@
+class SinglyLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def linkedListSearch(head,data):
+    index = 0
+    while head is not None:
+        if head.data is data:
+            return index
+        else:
+            index += 1
+        head = head.next
+    return -1
